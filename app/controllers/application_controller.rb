ad9d7e9fc9f06d81558b2a1f72a4ab38e62ecd61
Benard Haugen <ben.haugen@kellogg.northwestern.edu>
2025-03-15 19:55:19 +0000

    Starting work on controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    @current_user = User.find_by({ "id" => session["user_id"] })
+  end
+
+  def require_login
+    unless current_user
+      flash[:notice] = "Please login first."
+      redirect_to "/login"
+    end
   end
 end
